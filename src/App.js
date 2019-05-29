786ebd7c4641dba1dd4b414cb4af34befadfc83e
Ania Blaziak <anblaziak@gmail.com>
2019-05-29 15:03:33 +0100

    create basic html frame

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="header">
+        <h1 className="header__text">Sashay or Shantay?</h1>
       </header>
     </div>
   );
